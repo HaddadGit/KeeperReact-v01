5783dfaaf20b09cb21605d0bc42bc90bf8f11f23
Mohammad Haddad <66420319+HaddadGit@users.noreply.github.com>
2020-06-26 19:38:22 +0430

    Initial commit

---
+++ src/notes.js
@@ -0,0 +1,27 @@
+const notes = [
+  {
+    key: 1,
+    title: "title 1 Lorem ",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum leo vel orci porta non."
+  },
+  {
+    key: 2,
+    title: "title 2 Lorem ",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.      "
+  },
+  {
+    key: 3,
+    title: "title 3 Lorem ",
+    content:
+      "Id vidisse phaedrum convenire nec, audire consetetur ad sed. At pro tempor ponderum senserit. Ut eam natum primis perpetua, utinam lucilius ut vel. Quo ad idque homero gloriatur, qui illum dolor laudem ex, sed vocent insolens ne.      "
+  },
+  {
+    key: 4,
+    title: "title 4 Lorem ",
+    content:
+      "Cicero's version of Liber Primus (first Book), sections 1.10.32–3 (fragments included in most Lorem Ipsum variants in red ):      ."
+  }
+];
+export default notes;
